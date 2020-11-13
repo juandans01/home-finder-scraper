2fbcc27940a2b170e7f2a15b97c8d2b731282cdd
juandans01 <juandans01@gmail.com>
2020-11-12 22:08:44 -0300

    add loop and requirements.txt

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import time
 from scrapper import scrap
 
-# file = open('saved_properties.txt', 'r')
-# test = 'asdasd'
-
-  
-# print(to_save)
-# for line in :
-#   print(line)
-#   if not 
-# for line in open('saved_properties.txt', 'w+'):
-#   print(line)
-scrap()
-# while True:
-#   scrap()
-#   time.sleep(600)
+while True:
+  scrap()
+  time.sleep(600)
